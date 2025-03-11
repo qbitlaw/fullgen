1c50337c0122edf6a96f7ae32014733a08d83a22
qbitlaw <we.anonymous.us@gmail.com>
2025-03-12 03:12:19 +0545

    Add files via upload

---
+++ v2.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+# Define the characters and placement size
+characters = ['1', '2', '3', '4']
+placement_size = 5
+
+# Generate all possible combinations and write to file
+with open("results.txt", "w") as f:
+    for p in product(characters, repeat=placement_size):
+        f.write(''.join(p) + "\n")
